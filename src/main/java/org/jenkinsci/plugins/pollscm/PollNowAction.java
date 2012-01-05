5d4693694fdf3f342e87b140501684bae073e898
Vincent Latombe <vincent@latombe.net>
2012-01-05 17:58:06 +0100

    Initial commit of PollSCM plugin
    Change-Id: Ie376c9f7d3b8032c1c03fc97ea1b8fb4be1d2929

---
+++ src/main/java/org/jenkinsci/plugins/pollscm/PollNowAction.java
@@ -0,0 +1,79 @@
+/*
+ * The MIT License
+ * 
+ * Copyright (c) 2012, Vincent Latombe
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.jenkinsci.plugins.pollscm;
+
+
+import hudson.Extension;
+import hudson.model.Action;
+import hudson.model.TransientProjectActionFactory;
+import hudson.model.AbstractProject;
+import hudson.triggers.Trigger;
+import hudson.triggers.SCMTrigger;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class PollNowAction implements Action {
+	@Extension
+	public static class TransientProjectActionFactoryImpl extends TransientProjectActionFactory {
+
+		@Override
+		public Collection<? extends Action> createFor(AbstractProject target) {
+			Trigger trigger = target.getTrigger(SCMTrigger.class);
+			if (trigger != null) {
+				return Collections.singleton(new PollNowAction(target));
+			} else {
+				return Collections.EMPTY_LIST;
+			}
+		}
+
+	}
+	
+	private AbstractProject target;
+
+	public PollNowAction(AbstractProject target) {
+		this.target = target;
+	}
+	
+	public Trigger getTrigger() {
+		return target.getTrigger(SCMTrigger.class);
+	}
+	
+	public AbstractProject getOwner() {
+		return target;
+	}
+
+	public String getIconFileName() {
+		return "/plugin/pollscm/images/24x24/clipboard-play.png";
+	}
+
+	public String getDisplayName() {
+		return Messages.PollNowAction_PollNow();
+	}
+
+	public String getUrlName() {
+		return "poll";
+	}
+
+}
