614b5cfdb88dbf9b84312efa46dca39e641ae801
Mark Waite <mark.earl.waite@gmail.com>
2023-09-17 01:45:19 -0600

    Use automated code formatting
    Simplify maintenance by consistent code formatting

--- src/main/java/org/jenkinsci/plugins/pollscm/PollNowAction.java
+++ src/main/java/org/jenkinsci/plugins/pollscm/PollNowAction.java
@@ -1,18 +1,18 @@
 /*
  * The MIT License
- * 
+ *
  * Copyright (c) 2012-2017, Vincent Latombe
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -23,17 +23,6 @@
  */
 package org.jenkinsci.plugins.pollscm;
 
-
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-
-import org.kohsuke.accmod.Restricted;
-import org.kohsuke.accmod.restrictions.NoExternalUse;
-import org.kohsuke.stapler.StaplerRequest;
-import org.kohsuke.stapler.StaplerResponse;
-import org.kohsuke.stapler.interceptor.RequirePOST;
-
 import hudson.Extension;
 import hudson.model.Action;
 import hudson.model.Item;
@@ -41,16 +30,27 @@ import hudson.security.ACL;
 import hudson.security.Permission;
 import hudson.security.PermissionScope;
 import hudson.triggers.Trigger;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Collections;
+import javax.servlet.ServletException;
 import jenkins.model.Jenkins;
 import jenkins.model.TransientActionFactory;
 import jenkins.triggers.SCMTriggerItem;
+import org.kohsuke.accmod.Restricted;
+import org.kohsuke.accmod.restrictions.NoExternalUse;
+import org.kohsuke.stapler.StaplerRequest;
+import org.kohsuke.stapler.StaplerResponse;
+import org.kohsuke.stapler.interceptor.RequirePOST;
 
-import java.util.Collection;
-import java.util.Collections;
-
-@SuppressWarnings({ "rawtypes", "unchecked" })
+@SuppressWarnings({"rawtypes", "unchecked"})
 public class PollNowAction implements Action {
-    public static final Permission POLL = new Permission(Item.PERMISSIONS, "Poll", Messages._PollNowAction_PollPermission_Description(), Permission.UPDATE, PermissionScope.ITEM);
+    public static final Permission POLL = new Permission(
+            Item.PERMISSIONS,
+            "Poll",
+            Messages._PollNowAction_PollPermission_Description(),
+            Permission.UPDATE,
+            PermissionScope.ITEM);
     private SCMTriggerItem target;
 
     public PollNowAction(SCMTriggerItem target) {
@@ -91,7 +91,7 @@ public class PollNowAction implements Action {
      */
     @RequirePOST
     @Restricted(NoExternalUse.class)
-    public void doPolling(StaplerRequest req, StaplerResponse rsp ) throws IOException, ServletException {
+    public void doPolling(StaplerRequest req, StaplerResponse rsp) throws IOException, ServletException {
         getACL().checkPermission(POLL);
         Trigger trigger = getTrigger();
         if (trigger != null) {
@@ -119,5 +119,4 @@ public class PollNowAction implements Action {
             return SCMTriggerItem.class;
         }
     }
-
 }
--- src/test/java/org/jenkinsci/plugins/pollscm/PollNowActionTest.java
+++ src/test/java/org/jenkinsci/plugins/pollscm/PollNowActionTest.java
@@ -1,9 +1,13 @@
 package org.jenkinsci.plugins.pollscm;
 
-import hudson.Messages;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 import hudson.security.ACL;
 import hudson.triggers.SCMTrigger;
 import hudson.triggers.Trigger;
+import java.util.Collection;
+import java.util.Collections;
 import jenkins.model.Jenkins;
 import jenkins.triggers.SCMTriggerItem;
 import org.junit.Rule;
@@ -16,12 +20,6 @@ import org.kohsuke.stapler.StaplerResponse;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 
-import java.util.Collection;
-import java.util.Collections;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
-
 class PollNowActionTest {
     private PollNowAction mockPollNowAction;
     private SCMTriggerItem mockScmTriggerItem;
@@ -33,7 +31,7 @@ class PollNowActionTest {
     JenkinsRule jenkinsRule = new JenkinsRule();
 
     @BeforeEach
-    void setup(){
+    void setup() {
         mockScmTriggerItem = mock(SCMTriggerItem.class);
         mockScmTrigger = mock(SCMTrigger.class);
         jenkins = mock(Jenkins.class);
@@ -47,40 +45,40 @@ class PollNowActionTest {
     @Test
     void getTrigger() {
         Trigger trigger = mockPollNowAction.getTrigger();
-        assertEquals(mockScmTrigger,trigger);
+        assertEquals(mockScmTrigger, trigger);
     }
 
     @Test
     void getOwner() {
         SCMTriggerItem owner = mockPollNowAction.getOwner();
-        assertEquals(mockScmTriggerItem,owner);
+        assertEquals(mockScmTriggerItem, owner);
     }
 
     @Test
     void getIconFileNameWithPermission() {
-        try(MockedStatic<Jenkins> jenkinsMockedStatic = Mockito.mockStatic(Jenkins.class, CALLS_REAL_METHODS)){
-            jenkinsMockedStatic.when(()-> Jenkins.getInstance()).thenReturn(jenkins);
+        try (MockedStatic<Jenkins> jenkinsMockedStatic = Mockito.mockStatic(Jenkins.class, CALLS_REAL_METHODS)) {
+            jenkinsMockedStatic.when(() -> Jenkins.getInstance()).thenReturn(jenkins);
             when(acl.hasPermission(PollNowAction.POLL)).thenReturn(true);
             String iconName = mockPollNowAction.getIconFileName();
-            assertEquals("/plugin/pollscm/images/24x24/clipboard-play.png",iconName);
+            assertEquals("/plugin/pollscm/images/24x24/clipboard-play.png", iconName);
         }
     }
 
     @Test
     void getIconFileNameWithoutPermission() {
-        try(MockedStatic<Jenkins> jenkinsMockedStatic = Mockito.mockStatic(Jenkins.class, CALLS_REAL_METHODS)){
-            jenkinsMockedStatic.when(()-> Jenkins.getInstance()).thenReturn(jenkins);
+        try (MockedStatic<Jenkins> jenkinsMockedStatic = Mockito.mockStatic(Jenkins.class, CALLS_REAL_METHODS)) {
+            jenkinsMockedStatic.when(() -> Jenkins.getInstance()).thenReturn(jenkins);
             when(acl.hasPermission(PollNowAction.POLL)).thenReturn(false);
             String iconName = mockPollNowAction.getIconFileName();
-            assertEquals(null,iconName);
+            assertEquals(null, iconName);
         }
     }
 
     @Test
     @WithoutJenkins
-    void getIconFileNameIllegalExceptionHandler(){
-        try(MockedStatic<Jenkins> jenkinsMockedStatic = Mockito.mockStatic(Jenkins.class, CALLS_REAL_METHODS)){
-            assertThrows(IllegalStateException.class,()->mockPollNowAction.getIconFileName());
+    void getIconFileNameIllegalExceptionHandler() {
+        try (MockedStatic<Jenkins> jenkinsMockedStatic = Mockito.mockStatic(Jenkins.class, CALLS_REAL_METHODS)) {
+            assertThrows(IllegalStateException.class, () -> mockPollNowAction.getIconFileName());
         }
     }
 
@@ -93,37 +91,38 @@ class PollNowActionTest {
     @Test
     void getUrlName() {
         String url = mockPollNowAction.getUrlName();
-        assertEquals("poll",url);
+        assertEquals("poll", url);
     }
 
     @Test
     void doPolling() {
-        try(MockedStatic<Jenkins> jenkinsMockedStatic = Mockito.mockStatic(Jenkins.class, CALLS_REAL_METHODS)){
-            jenkinsMockedStatic.when(()-> Jenkins.getInstance()).thenReturn(jenkins);
+        try (MockedStatic<Jenkins> jenkinsMockedStatic = Mockito.mockStatic(Jenkins.class, CALLS_REAL_METHODS)) {
+            jenkinsMockedStatic.when(() -> Jenkins.getInstance()).thenReturn(jenkins);
             StaplerRequest req = mock(StaplerRequest.class);
             StaplerResponse res = mock(StaplerResponse.class);
             doNothing().when(mockScmTrigger).run();
 
-            assertDoesNotThrow(()->mockPollNowAction.doPolling(req,res));
-            verify(mockScmTrigger,times(1)).run();
+            assertDoesNotThrow(() -> mockPollNowAction.doPolling(req, res));
+            verify(mockScmTrigger, times(1)).run();
         }
     }
 
     @Test
     void doPollingThatThrowsException() {
-        try(MockedStatic<Jenkins> jenkinsMockedStatic = Mockito.mockStatic(Jenkins.class, CALLS_REAL_METHODS)){
-            jenkinsMockedStatic.when(()-> Jenkins.getInstance()).thenReturn(jenkins);
+        try (MockedStatic<Jenkins> jenkinsMockedStatic = Mockito.mockStatic(Jenkins.class, CALLS_REAL_METHODS)) {
+            jenkinsMockedStatic.when(() -> Jenkins.getInstance()).thenReturn(jenkins);
             StaplerRequest req = mock(StaplerRequest.class);
             StaplerResponse res = mock(StaplerResponse.class);
             when(mockPollNowAction.getTrigger()).thenReturn(null);
 
-            assertThrows(IllegalStateException.class,()->mockPollNowAction.doPolling(req,res));
+            assertThrows(IllegalStateException.class, () -> mockPollNowAction.doPolling(req, res));
         }
     }
 
     @Test
-    void createForWithException(){
-        PollNowAction.TransientProjectActionFactoryImpl transientProjectActionFactory = new PollNowAction.TransientProjectActionFactoryImpl();
+    void createForWithException() {
+        PollNowAction.TransientProjectActionFactoryImpl transientProjectActionFactory =
+                new PollNowAction.TransientProjectActionFactoryImpl();
         when(mockScmTriggerItem.getSCMTrigger()).thenReturn(null);
         Collection collection = transientProjectActionFactory.createFor(mockScmTriggerItem);
 
@@ -131,8 +130,9 @@ class PollNowActionTest {
     }
 
     @Test
-    void createForWithoutException(){
-        PollNowAction.TransientProjectActionFactoryImpl transientProjectActionFactory = new PollNowAction.TransientProjectActionFactoryImpl();
+    void createForWithoutException() {
+        PollNowAction.TransientProjectActionFactoryImpl transientProjectActionFactory =
+                new PollNowAction.TransientProjectActionFactoryImpl();
         when(mockScmTriggerItem.getSCMTrigger()).thenReturn(mockScmTrigger);
         Collection collection = transientProjectActionFactory.createFor(mockScmTriggerItem);
 
@@ -140,8 +140,9 @@ class PollNowActionTest {
     }
 
     @Test
-    void getClassTest(){
-        PollNowAction.TransientProjectActionFactoryImpl transientProjectActionFactory = new PollNowAction.TransientProjectActionFactoryImpl();
-        assertEquals(SCMTriggerItem.class,transientProjectActionFactory.type());
+    void getClassTest() {
+        PollNowAction.TransientProjectActionFactoryImpl transientProjectActionFactory =
+                new PollNowAction.TransientProjectActionFactoryImpl();
+        assertEquals(SCMTriggerItem.class, transientProjectActionFactory.type());
     }
 }
